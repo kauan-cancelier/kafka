e47e9d7b7bad47e6b5cb14c5db11e7c87f5f7215
Kauan Mello Cancelier <kauan.cancelier@nuntec.com.br>
2024-10-17 15:10:53 -0300

    str-producer started

---
+++ str-producer/src/test/java/com/kauan_cancelier/str_producer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauan_cancelier.str_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
